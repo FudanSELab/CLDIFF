2d484f0bcd7daf8cb56c4e36a1395bc40693b665
Yelloman <cavinkfh@gmail.com>
2017-11-21 21:13:55 +0800

    MyActionClusterFinder and MyActionGenerator

--- src/edu/fdu/se/ast/MyASTParser.java
+++ src/edu/fdu/se/ast/MyASTParser.java
@@ -25,48 +25,9 @@ public class MyASTParser {
 	
 	public static void main(String args[]){
 		
-		String b="bbb";
-		List<String> aa=new ArrayList<String>();
-		aa.add(b);
-		System.out.println(aa);
-//		System.out.println(System.getProperty("gt.stm.mh", "1"));
-//		String file1 = "C:\\Users\\huangkaifeng\\Desktop\\11-8\\test\\curr\\RefactorMoveMethod.java";
-//		String file2 = "C:\\Users\\huangkaifeng\\Desktop\\11-8\\test\\prev\\RefactorMoveMethod.java";
-//		Run.initGenerators();
-//		ITree src=null;
-//		ITree dst=null;
-//		TreeContext tsrc=null;
-//		TreeContext tdst=null;
-//		try {
-//			tsrc = new JdtTreeGenerator().generateFromFile(new File(file1));
-//			tdst = new JdtTreeGenerator().generateFromFile(new File(file2));
-//			src = tsrc.getRoot();
-//			dst = tdst.getRoot();
-//		} catch (UnsupportedOperationException | IOException e) {
-//			e.printStackTrace();
-//		}
-//		Matcher m = Matchers.getInstance().getMatcher(src, dst); // retrieve the default matcher
-//		m.match();
-//		MappingStore mappings = m.getMappings();
-//		RootAndLeavesClassifier ral = new RootAndLeavesClassifier(tsrc,tdst,m);
-//		ActionGenerator g = new ActionGenerator(src, dst, mappings);
-//		g.generate();
-//		List<Action> actions = g.getActions();
-//		ActionClusterFinder finder = new ActionClusterFinder(tsrc, tdst, actions);
-//		finder.show();
-
-//		Map<ITree,ITree> mm = mappings.getDsts();
-//		for(Entry<ITree,ITree> item:mm.entrySet()){
-//			System.out.println(item.getKey().toShortString()+"    "+item.getValue().toShortString());
-//		}
-		
-		
-		
+		String a1="AA";
+		String a2="BB";
+		System.out.println(a1 instanceof String);
 	}
-	
-	
-	
-
-	
 
 }
--- src/edu/fdu/se/ast/MyGumTreeParser.java
+++ src/edu/fdu/se/ast/MyGumTreeParser.java
@@ -1,26 +1,51 @@
 package edu.fdu.se.ast;
 
+import java.util.List;
+
 import com.github.gumtreediff.actions.ActionUtil;
+import com.github.gumtreediff.actions.model.Action;
+import com.github.gumtreediff.matchers.MappingStore;
 import com.github.gumtreediff.tree.TreeContext;
 
-import edu.fdu.se.ast.GumTreeDiffParser;
+import cn.edu.fudan.se.apiChangeExtractor.gumtreeParser.GumTreeDiffParser;
 import edu.fdu.se.fileutil.FileUtil;
 
 public class MyGumTreeParser {
-	public static void main(String args[]){
-		String file2 = "C:\\Users\\huangkaifeng\\Desktop\\11-8\\test\\curr\\GTExample3.java";
-		String file1 = "C:\\Users\\huangkaifeng\\Desktop\\11-8\\test\\prev\\GTExample3.java";
+	//TODO check
+	public void checkIfAppliedActionOnSrcSameAsDst(){
+		String file2 = "C:\\Users\\huangkaifeng\\Desktop\\11-8\\test\\curr\\GTExample2.java";
+		String file1 = "C:\\Users\\huangkaifeng\\Desktop\\11-8\\test\\prev\\GTExample2.java";
 		GumTreeDiffParser diff = new GumTreeDiffParser(file1,file2);
 		diff.init();
 		FileUtil.writeInAll("C:/Users/huangkaifeng/Desktop/11-8/srcTree.txt",diff.getPrettyOldTreeString());
 		FileUtil.writeInAll("C:/Users/huangkaifeng/Desktop/11-8/dstTree.txt",diff.getPrettyNewTreeString());
 		System.out.println("----------------------Actions----------------------------------");
 		diff.printActions(diff.getActions());
-		TreeContext temp = ActionUtil.apply(diff.srcTC, diff.actions);
+		TreeContext temp = ActionUtil.apply(diff.srcTC, diff.getActions());
 		diff.srcTC.validate();
 		FileUtil.writeInAll("C:/Users/huangkaifeng/Desktop/11-8/dstnewTree.txt",diff.getPrettyOldTreeString());
-		
 //		diff.listStartNodes();
+	}
+	public MappingStore mMapping;
+	public List<Action> mActions;
+	
+	public void userGTIntermediate(){
+		String file1 = "C:\\Users\\huangkaifeng\\Desktop\\11-8\\test\\prev\\GTExample2.java";
+		String file2 = "C:\\Users\\huangkaifeng\\Desktop\\11-8\\test\\curr\\GTExample2.java";
+		GumTreeDiffParser his = new GumTreeDiffParser(file1,file2);
+		his.init();
+		mMapping = his.getMapping();
+		mActions = his.getActions();
+		FileUtil.writeInAll("C:/Users/huangkaifeng/Desktop/11-8/srcTree.txt",his.getPrettyOldTreeString());
+		FileUtil.writeInAll("C:/Users/huangkaifeng/Desktop/11-8/dstTree.txt",his.getPrettyNewTreeString());
+		System.out.println("----------------------------Actions----------------------------------");
+		his.printActions(his.getActions());
+//		diff.listStartNodes();
+		
+	}
+	public static void main(String args[]){
+		MyGumTreeParser i = new MyGumTreeParser();
+		i.userGTIntermediate();
 	}
 
 }
