6b9efe6b175453b23f95b7a3146424b64f941913
Yelloman <cavinkfh@gmail.com>
2017-11-22 09:35:32 +0800

    update

--- src/edu/fdu/se/ast/MyGumTreeParser.java
+++ src/edu/fdu/se/ast/MyGumTreeParser.java
@@ -1,5 +1,6 @@
 package edu.fdu.se.ast;
 
+import java.io.File;
 import java.util.List;
 
 import com.github.gumtreediff.actions.ActionUtil;
@@ -20,15 +21,67 @@ public class MyGumTreeParser extends GumTreeDiffParser{
 		super(oldFileName, newFileName);
 	}
 	public void printClusteredActions(List<List<Action>> actionSet){
-		StringBuilder sb = new StringBuilder();
+		String actionOutputFile = "C:/Users/huangkaifeng/Desktop/11-8/clusteredAction.txt";
+		File mFile = new File(actionOutputFile);
+		FileUtil.writeInSegments(mFile,"start:----------\n",1);
 		for(List<Action> oneEntry:actionSet){
 			boolean isFirst=true;
+			FileUtil.writeInSegments(mFile,"Cluter:----------\n",1);
 			for(Action a:oneEntry){
 				if(isFirst==true){
 					isFirst=false;
 				}
+				String oneAction = this.printMyOneActionString(a);
+				FileUtil.writeInSegments(mFile, oneAction, 1);
 			}
+			FileUtil.writeInSegments(mFile, "----end", 1);
 		}
+		FileUtil.writeInSegments(mFile, "--------", 2);
+	}
+	public String printMyOneActionString(Action a){
+		StringBuilder sb = new StringBuilder();
+		if(a instanceof Delete){
+			sb.append("Delete>");
+			Delete delete = (Delete)a;
+			ITree deleteNode = delete.getNode();
+			sb.append(prettyString(dstTC,deleteNode)+" from "+prettyString(dstTC,deleteNode.getParent())+"\n");
+			sb.append(delete.toString()+"\n");
+		}
+		if(a instanceof Insert){
+			sb.append("Insert>");
+			Insert insert = (Insert)a;
+			ITree insertNode = insert.getNode();
+			sb.append(prettyString(dstTC,insertNode)+" to "+prettyString(dstTC,insertNode.getParent())+" at "+ insert.getPosition()+"\n");
+			sb.append(insert.toString()+"\n");
+			sb.append("----------------Insert.getParent(Old Tree)----------------------------");
+			sb.append(prettyString(dstTC, insert.getParent())+"\n");//old
+		}
+		if(a instanceof Move){
+			sb.append("Move>");
+			Move move = (Move)a;
+			ITree moveNode = move.getNode();
+			sb.append(prettyString(dstTC,moveNode)+" to "+prettyString(dstTC,move.getParent())+" at "+ move.getPosition()+"\n");//old
+			sb.append(move.toString()+"\n");
+			sb.append(move.getParent()==move.getNode().getParent());
+
+			sb.append("\n----------------Move.getParent(New Tree)----------------------------");
+			sb.append(prettyString(dstTC, move.getParent())+"\n");//new
+		}
+		if(a instanceof Update){
+			sb.append("Update>");
+			Update update = (Update)a;
+			ITree updateNode = update.getNode();
+			sb.append("from "+updateNode.getLabel()+" to "+update.getValue()+"\n");//old
+		}
+		sb.append("----------------Action.getNode----------------------------");
+		sb.append(prettyString(dstTC, a.getNode())+"\n");
+		//move-old,update-old,insert-new,delete-old
+//		sb.append(toTreeString(dstTC, a.getNode()));
+		sb.append("-----------------Action.getNode.getParent---------------------------");
+		sb.append(prettyString(dstTC, a.getNode().getParent())+"\n");//move-old,update-old,insert-new,delete-old
+//		sb.append(toTreeString(dstTC, a.getNode().getParent()));
+		sb.append("============================================\n");
+		return sb.toString();
 	}
 	
 
--- src/edu/fdu/se/fileutil/FileUtil.java
+++ src/edu/fdu/se/fileutil/FileUtil.java
@@ -1,20 +1,52 @@
 package edu.fdu.se.fileutil;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FileUtil {
-	
-	public static void writeInAll(String filePath,String content){
+
+	public static void writeInAll(String filePath, String content) {
 		try {
 			FileOutputStream fos = new FileOutputStream(filePath);
 			fos.write(content.getBytes());
 			fos.close();
-		} catch ( IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
+	}
+
+	private static Map<File, FileOutputStream> fileMap = new HashMap<File, FileOutputStream>();
+
+	/**
+	 * 
+	 * @param mFile
+	 * @param content
+	 * @param flag
+	 *            1:normal 2:write end
+	 */
+	public static void writeInSegments(File mFile, String content, int flag) {
+		try {
+			if (fileMap.containsKey(mFile)) {
+				FileOutputStream fos = fileMap.get(mFile);
+				fos.write(content.getBytes());
+
+			} else {
+				FileOutputStream fos = new FileOutputStream(mFile);
+				fos.write(content.getBytes());
+				fileMap.put(mFile, fos);
+			}
+			if(flag==2){
+				fileMap.get(mFile).close();
+				fileMap.remove(mFile);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
