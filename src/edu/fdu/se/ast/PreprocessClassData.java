1439e91c60aa06077fb7d79a535b4c54aca0f3fb
clewang <clewang@163.com>
2017-11-27 09:59:10 +0800

    classdata

--- src/edu/fdu/se/ast/PreprocessClassData.java
+++ src/edu/fdu/se/ast/PreprocessClassData.java
@@ -4,27 +4,26 @@ import java.util.List;
 import java.util.Map;
 
 public class PreprocessClassData {
-	
-	public Map<String,String> sameMethod;
-	
-	public Map<String,String> sameField;
-	
+
+	public Map<String, String> sameMethod;
+
+	public Map<String, String> sameField;
+
 	/**
-	 *  same method name, key:method signature,value:list[0]=prev list[1]=curr
+	 * same method name, key:method signature,value:list[0]=prev list[1]=curr
 	 */
-	public Map<String,List<String>> differentMethodsMap;
+	public Map<String, List<String>> differentMethodsMap;
 	/**
 	 * different method name
 	 */
-	public Map<String,String> prevDeletedMethods;
-	public Map<String,String> currInsertedMethods;
-	
+	public Map<String, String> prevDeletedMethods;
+	public Map<String, String> currInsertedMethods;
+
 	/**
 	 * field
 	 */
-	public Map<String,String> prevDeletedFields;
-	
-	public Map<String,String> currInsertedFields;
-	
+	public Map<String, String> prevDeletedFields;
+
+	public Map<String, String> currInsertedFields;
 
 }
