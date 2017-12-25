7ad0684613d01c236179b8f48ed9566e8b195c78
calvinhkf <cavinkfh@gmail.com>
2017-12-25 10:40:07 +0800

    update,undo delete

--- src/edu/fdu/se/main/androidrepo/GroundTruthFinder.java
+++ src/edu/fdu/se/main/androidrepo/GroundTruthFinder.java
@@ -33,28 +33,26 @@ import edu.fdu.se.git.commitcodeinfo.CommitCodeInfo;
 import edu.fdu.se.git.commitcodeinfo.FileChangeEditList;
 import edu.fdu.se.gitrepo.RepoConstants;
 import edu.fdu.se.javaparser.JavaParserFactory;
-
+/**
+ * 输入tag标签，tag标签之前的candidate commit
+ * @author huangkaifeng
+ *
+ */
 public class GroundTruthFinder {
 	
 	public GroundTruthFinder(){
 		tagCmd = new JGitTagCommand(
 				ProjectProperties.getInstance().getValue(PropertyKeys.ANDROID_REPO_PATH2)
 				+ RepoConstants.platform_frameworks_base_ + ".git");
-		this.releaseCommitList = new ArrayList<RevCommit>();
-		this.tagList = new ArrayList<String>();
-		this.timeStart = 0;
-		this.timeEnd =0;
+		this.commitAndTagMap = new HashMap<RevCommit,String>();
 	}
 	
 	JGitTagCommand tagCmd;
-	List<RevCommit> releaseCommitList;
-	List<String> tagList;
-	int timeStart;
-	int timeEnd;
+	Map<RevCommit,String> commitAndTagMap;
+	
 	public static void main(String args[]){
 		GroundTruthFinder run = new GroundTruthFinder();
 		run.addReleaseCommitByTagName("android-4.0.3_r1");
-		run.timeStart = run.releaseCommitList.get(0).getCommitTime();
 		run.addReleaseCommitByTagName("android-4.1.1_r1");
 		run.addReleaseCommitByTagName("android-4.2_r1");
 		run.addReleaseCommitByTagName("android-4.3_r1");
@@ -65,7 +63,6 @@ public class GroundTruthFinder {
 		run.addReleaseCommitByTagName("android-7.0.0_r1");
 		run.addReleaseCommitByTagName("android-7.1.0_r1");
 		run.addReleaseCommitByTagName("android-8.0.0_r1");
-		run.timeStart = run.releaseCommitList.get(run.releaseCommitList.size()-1).getCommitTime();
 		run.run();
 	}
 	private void addReleaseCommitByTagName(String tagName){
@@ -78,8 +75,17 @@ public class GroundTruthFinder {
 				ProjectProperties.getInstance().getValue(PropertyKeys.ANDROID_REPO_PATH2)
 				+ RepoConstants.platform_frameworks_base_ + ".git");
 		RevCommit revCommit = tagCmd.revCommitOfTag(mTag.getTagShaId());
-		this.releaseCommitList.add(revCommit);
-		this.tagList.add(tagName);
+		this.commitAndTagMap.put(revCommit, tagName);
+	}
+	
+	private List<RevCommit> chooseCandidateTag(int commitTime){
+		List<RevCommit> candidateCommit = new ArrayList<RevCommit>();
+		for(RevCommit commit:this.commitAndTagMap.keySet()){
+			if(commit.getCommitTime()<commitTime){
+				candidateCommit.add(commit);
+			}
+		}
+		return candidateCommit;
 	}
 	
 	
@@ -97,34 +103,52 @@ public class GroundTruthFinder {
 		String line ;
 		while((line = br.readLine())!=null){
 			line = line.trim();
+			System.out.println(line);
 			int commitTime = tagCmd.readCommitTime(line);
-			if(commitTime<mTime){
-				System.err.println("early ignore");
+			List<RevCommit> candidate = this.chooseCandidateTag(commitTime);
+			if(candidate.size()==0){
+				System.out.println("early skip");
+				continue;
 			}
 			CommitCodeInfo mCCI = tagCmd.getCommitEditSummary(line);
 			Map<RevCommit,List<FileChangeEditList>> mMap = mCCI.getFileDiffEntryMap();
 			for(Entry<RevCommit,List<FileChangeEditList>> item :mMap.entrySet()){
 				RevCommit parent = item.getKey();
 				List<FileChangeEditList> list = item.getValue();
+				System.out.println("\tParent");
+				List<String> tagNameListAll =null;
 				for(FileChangeEditList item2 :list){
 					//每一个文件 都去比较一次
 					String oldPath = item2.getOldFilePath();
 					String fileName = getClassNameFromPath(oldPath);
+					//commit change的文件
 					System.out.println(oldPath);
-					Map<String,List<MethodDeclaration>> codeFromRelease = this.mappingTagStrToMethodDeclarationList(oldPath, fileName);
+					Map<String,List<MethodDeclaration>> codeFromRelease = this.mappingTagStrToMethodDeclarationList(oldPath, fileName,candidate);
 					
 					InputStream fileInputStream = tagCmd.extractAndReturnInputStream(oldPath, parent.getName());
 					List<MethodDeclaration> codePrev = JavaParserFactory.parseInputStreamGetOverlapMethodDeclarationList(fileInputStream, fileName, item2);
-					this.compareTwoSet(codeFromRelease,codePrev);
+					List<String> tagNameList = this.compareTwoSet(codeFromRelease,codePrev);
+					if(tagNameList==null){
+						tagNameListAll = tagNameList;
+					}else{
+						tagNameListAll.retainAll(tagNameList);
+					}
 				}
+				if(tagNameListAll!=null){
+					for(String tmp:tagNameListAll){
+						System.out.println(tmp);
+					}
+				}else{
+					System.out.println("No Tag commit matched");
+				}
+				
 			}
 			
 		}
+		br.close();
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -133,29 +157,43 @@ public class GroundTruthFinder {
 		return fileName.substring(0, fileName.length()-5);
 	}
 	
-	public void compareTwoSet(Map<String,List<MethodDeclaration>> releasedTagMethod,List<MethodDeclaration> mList){
+	public List<String> compareTwoSet(Map<String,List<MethodDeclaration>> releasedTagMethod,List<MethodDeclaration> mList){
+		List<String> matchedTagList = new ArrayList<String>();
 		for(Entry<String,List<MethodDeclaration>> item: releasedTagMethod.entrySet()){
 			String tagName = item.getKey();
 			List<MethodDeclaration> mList2 = item.getValue();
-			for(MethodDeclaration tagRelease:mList2){
-				for(MethodDeclaration changeCode:mList){
-					if(tagRelease.getDeclarationAsString().equals(changeCode.getDeclarationAsString())){
-						if(tagRelease.getBody().get().toString().equals(changeCode.getBody().get().toString())){
+			boolean isTagCommitMatch = true;
+			for(MethodDeclaration changeCode:mList){
+				boolean isMatch = false;
+				for(MethodDeclaration tagRelease:mList2){
+					if(tagRelease.getDeclarationAsString().equals(changeCode.getDeclarationAsString()) 
+							&& tagRelease.getBody().get().toString().equals(changeCode.getBody().get().toString())){
 							System.out.println("mapped");
-						}
+							isMatch = true;
+							break;
 					}
 				}
+				// isMatch false没找到对应
+				if(!isMatch){
+					isTagCommitMatch = false;
+					break;
+				}
+				// true 找到对应的
+			}
+			if(isTagCommitMatch){
+				matchedTagList.add(tagName);
 			}
 		}
+		return matchedTagList;
 	}
 	
-	public Map<String,List<MethodDeclaration>> mappingTagStrToMethodDeclarationList(String oldPath,String className){
+	public Map<String,List<MethodDeclaration>> mappingTagStrToMethodDeclarationList(String oldPath,String className,List<RevCommit> candidate){
 		Map<String,List<MethodDeclaration>> result = new HashMap<String,List<MethodDeclaration>>();
 		
-		for(int i=0;i<this.tagList.size();i++){
-			InputStream fileInputStream  = tagCmd.extractAndReturnInputStream(oldPath, this.releaseCommitList.get(i).getName());
+		for(int i=0;i<candidate.size();i++){
+			InputStream fileInputStream  = tagCmd.extractAndReturnInputStream(oldPath, candidate.get(i).getName());
 			List<MethodDeclaration> mList = JavaParserFactory.parseFileGetAllMethodDeclaration(fileInputStream, className);
-			result.put(this.tagList.get(i), mList);
+			result.put(this.commitAndTagMap.get(candidate.get(i)), mList);
 		}
 		return result;
 	}
