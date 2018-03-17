6b70082c88ee6d81abb7e7fa108d10a96d9061ab
calvinhkf <cavinkfh@gmail.com>
2018-03-17 20:55:37 +0800

    link step 1

--- src/edu/fdu/se/astdiff/linkpool/LinkBean.java
+++ src/edu/fdu/se/astdiff/linkpool/LinkBean.java
@@ -2,6 +2,14 @@ package edu.fdu.se.astdiff.linkpool;
 
 
 
+import com.github.gumtreediff.actions.model.Action;
+import com.github.gumtreediff.tree.Tree;
+import edu.fdu.se.astdiff.miningoperationbean.ClusteredActionBean;
+import org.eclipse.jdt.core.dom.*;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -12,11 +20,103 @@ public class LinkBean {
 
     public Set<String> variables;
 
-    public Set<String> methodCalls;
+    public Set<String> methodNames;
 
     public Set<String> methodDeclarations;
 
+    /**
+     * 针对预处理的BodyDeclaraiton 的情况
+     */
     public LinkBean(){
 
+    }
+
+    /**
+     * 针对Bean的情况
+     */
+    public LinkBean(ClusteredActionBean bean){
+        List<Action> actions = bean.actions;
+        Set<String> simpleNameList = new HashSet<>();
+        for(Action a:actions){
+            Tree tree = (Tree)a.getNode();
+            if(tree.getAstNode().getClass().getSimpleName().equals("SimpleName")|| tree.getAstNode().getClass().getSimpleName().endsWith("Literal")){
+                simpleNameList.add(tree.getLabel());
+            }
+        }
+        for(Action a:actions){
+            Tree tree = (Tree)a.getNode();
+            if(tree.getAstNode().getClass().getSimpleName().equals("SimpleName")|| tree.getAstNode().getClass().getSimpleName().endsWith("Literal")){
+                ASTNode methodInvocation = findMethodInvoation(tree);
+                if(methodInvocation == null){
+                    continue;
+                }
+                List<List<String>> mList = methodInvocationList((MethodInvocation) methodInvocation);
+                this.methodNames.addAll(mList.get(0));
+                this.variables.addAll(mList.get(1));
+            }
+        }
+    }
+
+    private ASTNode findMethodInvoation(Tree tree){
+        while(tree.getAstNode().getClass().getSimpleName().endsWith("Declaration")){
+            tree = (Tree)tree.getParent();
+            if(tree.getAstNode().getNodeType() == ASTNode.METHOD_INVOCATION){
+                return tree.getAstNode();
+            }
+        }
+        return null;
+    }
+
+    private List<List<String>> methodInvocationList(MethodInvocation methodInvocation){
+        List<List<String>> result = new ArrayList<>();
+        String methodName = methodInvocation.getName().toString();
+        List<String> methodNameList = new ArrayList<>();
+        List<String> varNameList = new ArrayList<>();
+        methodNameList.add(methodName);
+        List arguments = methodInvocation.arguments();
+        for(int i =0;i<arguments.size();i++){
+            ASTNode tmp = (ASTNode) arguments.get(i);
+            switch(tmp.getNodeType()){
+                case ASTNode.METHOD_INVOCATION:
+                    List<List<String>> result2 = methodInvocationList((MethodInvocation)tmp);
+                    methodNameList.addAll(result2.get(0));
+                    varNameList.addAll(result2.get(1));
+                    break;
+                case ASTNode.SIMPLE_NAME:
+                    SimpleName simpleName = (SimpleName)tmp;
+                    varNameList.add(simpleName.toString());
+                    break;
+                case ASTNode.BOOLEAN_LITERAL:
+                    BooleanLiteral bl = (BooleanLiteral)tmp;
+                    varNameList.add(bl.toString());
+                    break;
+                case ASTNode.CHARACTER_LITERAL:
+                    CharacterLiteral cl = (CharacterLiteral)tmp;
+                    varNameList.add(cl.toString());
+                    break;
+                case ASTNode.NULL_LITERAL:
+                    NullLiteral nl = (NullLiteral)tmp;
+                    varNameList.add(nl.toString());
+                    break;
+                case ASTNode.NUMBER_LITERAL:
+                    NumberLiteral nll = (NumberLiteral) tmp;
+                    varNameList.add(nll.toString());
+                    break;
+                case ASTNode.STRING_LITERAL:
+                    StringLiteral sl = (StringLiteral)tmp;
+                    varNameList.add(sl.toString());
+                    break;
+                case ASTNode.TYPE_LITERAL:
+                    TypeLiteral tl = (TypeLiteral) tmp;
+                    varNameList.add(tl.toString());
+                    break;
+                default:
+                    System.err.println("UNExpdasdsad");
+                    break;
+            }
+        }
+        result.add(methodNameList);
+        result.add(varNameList);
+        return result;
     }
 }
--- src/edu/fdu/se/astdiff/preprocessingfile/TestFilePre.java
+++ src/edu/fdu/se/astdiff/preprocessingfile/TestFilePre.java
@@ -1,5 +1,6 @@
 package edu.fdu.se.astdiff.preprocessingfile;
 
+import org.apache.ibatis.javassist.compiler.ast.MethodDecl;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.ITypeRoot;
 import org.eclipse.jdt.core.dom.*;
@@ -18,31 +19,28 @@ public class TestFilePre {
         try {
             ASTParser astParser = ASTParser.newParser(AST.JLS8); // 非常慢
 //            String file = "D:/Workspace/Android_Diff/SDK_Files_15-26/android-25/android/accessibilityservice/AccessibilityService.java";
-            String file = "C:\\Users\\huangkaifeng\\Desktop\\a\\Test.java";
+            String file = "C:\\Users\\huangkaifeng\\Desktop\\Test.java";
             BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(file));
             byte[] input = new byte[bufferedInputStream.available()];
             bufferedInputStream.read(input);
             bufferedInputStream.close();
             astParser.setSource(new String(input).toCharArray());
-            CompilationUnit result = (CompilationUnit) (astParser
-                    .createAST(null));
+            CompilationUnit result = (CompilationUnit) (astParser.createAST(null));
             removeAllCommentsOfCompilationUnit(result);
             List<ASTNode> list =  result.types();
             TypeDeclaration typeDeclaration = (TypeDeclaration) list.get(0);
             List<ASTNode> bodies = typeDeclaration.bodyDeclarations();
-            ASTNode aaa = null;
-            for(ASTNode a:bodies){
-                System.out.println(a.getClass().getSimpleName()+" " + result.getLineNumber(a.getStartPosition()) +","+result.getLineNumber(a.getStartPosition()+a.getLength()-1));
-//                System.out.println(a.toString());
-                if(a.getClass().getSimpleName().equals("TypeDeclaration")){
-                	aaa = a;
-                }
-            }
-            aaa.delete();
-            System.out.println("-------------------");
-            bodies = typeDeclaration.bodyDeclarations();
-            for(ASTNode a:bodies){
-            	System.out.println(a.getClass().getSimpleName()+" " + result.getLineNumber(a.getStartPosition()) +","+result.getLineNumber(a.getStartPosition()+a.getLength()-1));
+            MethodDeclaration method =(MethodDeclaration) bodies.get(0);
+            List<Statement> stmts =  method.getBody().statements();
+            for(Statement s:stmts){
+                ExpressionStatement es = (ExpressionStatement) s;
+                MethodInvocation e = (MethodInvocation) es.getExpression();
+                String a = e.getName().toString();
+                String b = e.getExpression().toString();
+                List arguments = e.arguments();
+                Expression ee = e.getExpression();
+                FieldAccess fd = (FieldAccess) ee;
+                System.out.println(a+ ","+b);
             }
         }catch(Exception e){
             e.printStackTrace();
