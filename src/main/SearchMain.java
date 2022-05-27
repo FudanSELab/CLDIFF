85376c190ff6da68f5556a38ec81236b1b798338
Weichong Ling <vincentling3gz@gmail.com>
2022-05-27 00:12:12 -0700

    Tokenized regex generator done

--- src/main/SearchMain.java
+++ src/main/SearchMain.java
@@ -1,64 +1,64 @@
 package main;
 
-import edu.ucla.se.GenerateRegex;
-
 import java.util.*;
 
 import edu.ucla.se.MissingChangeInfo;
 import edu.ucla.se.P_LANG;
+import edu.ucla.se.RegexGenerator;
 import edu.ucla.se.SearchEngine;
 
 import java.nio.file.Paths;
 
 public class SearchMain {
     public static void main(String[] args) {
-//        System.out.println("Start running search...");
+        System.out.println("Start running search...");
+
+        HashMap<String, List<Integer>> fileToLine = new HashMap<>();
+        fileToLine.put("a.java", Arrays.asList(7,8));
+        fileToLine.put("b.java", Arrays.asList(7,8));
+
+        HashMap<Integer, HashMap<String, List<Integer>>> group = new HashMap<>();
+        group.put(1, fileToLine);
+
+        HashMap<Integer, String> a = new HashMap<>();
+        a.put(7, "else if (a && b) {");
+        a.put(8, "b++;");
+
+        HashMap<Integer, String> b = new HashMap<>();
+        b.put(7, "else if (a && d) {");
+        b.put(8, "d++;");
+
+        HashMap<String, HashMap<Integer, String>> dict = new HashMap<>();
+        dict.put("a.java", a);
+        dict.put("b.java", b);
+
+        RegexGenerator generator = new RegexGenerator(group, dict);
+        ArrayList<String> regex = generator.generateRegex();
+        System.out.println(regex);
+
+
+//        if (args.length != 2) {
+//            System.err.println("Usage [Repo Path] [Commit Id].\n");
+//            System.exit(1);
+//        }
 //
-//        HashMap<String, List<Integer>> fileToLine = new HashMap<>();
-//        fileToLine.put("a.java", Arrays.asList(7,8));
-//        fileToLine.put("b.java", Arrays.asList(7,8));
+//        String repoPath = Paths.get(args[0]).toString();
+//        String commitId = args[1];
 //
-//        HashMap<Integer, HashMap<String, List<Integer>>> group = new HashMap<>();
-//        group.put(1, fileToLine);
+//        System.out.printf("Start missing change search for git repo %s and commit %s...\n", repoPath, commitId);
 //
-//        HashMap<Integer, String> a = new HashMap<>();
-//        a.put(7, "event.item = item;");
-//        a.put(8, "sendEvent(true, event);");
+//        SearchEngine engine = new SearchEngine(repoPath, commitId, P_LANG.JAVASCRIPT);
 //
-//        HashMap<Integer, String> b = new HashMap<>();
-//        b.put(7, "event.item = item;");
-//        b.put(8, "sendEvent(true, ev);");
+//        List<String> regex = new ArrayList<>();
+//        regex.add(".*const.*\r\n.*db.*");
 //
-//        HashMap<String, HashMap<Integer, String>> dict = new HashMap<>();
-//        dict.put("a.java", a);
-//        dict.put("b.java", b);
+//        Map<String, List<MissingChangeInfo>> possibleRs = engine.run(regex);
 //
-//        GenerateRegex generator = new GenerateRegex(group, dict);
-//        HashMap<Integer,ArrayList<ArrayList<String>>> code = generator.getCodeSnippet();
-//        System.out.println(code);
-
-        if (args.length != 2) {
-            System.err.println("Usage [Repo Path] [Commit Id].\n");
-            System.exit(1);
-        }
-
-        String repoPath = Paths.get(args[0]).toString();
-        String commitId = args[1];
-
-        System.out.printf("Start missing change search for git repo %s and commit %s...\n", repoPath, commitId);
-
-        SearchEngine engine = new SearchEngine(repoPath, commitId, P_LANG.JAVASCRIPT);
-
-        List<String> regex = new ArrayList<>();
-        regex.add(".*const.*\r\n.*db.*");
-
-        Map<String, List<MissingChangeInfo>> possibleRs = engine.run(regex);
-
-        for (Map.Entry<String, List<MissingChangeInfo>> entry : possibleRs.entrySet()) {
-            System.out.printf("Possible Missing Changes in %s:\n", entry.getKey());
-            for (MissingChangeInfo info : entry.getValue())
-                System.out.printf("(Line%d, Line%d), %s\n", info.getStartLine(), info.getEndLine(), info.getContent());
-        }
+//        for (Map.Entry<String, List<MissingChangeInfo>> entry : possibleRs.entrySet()) {
+//            System.out.printf("Possible Missing Changes in %s:\n", entry.getKey());
+//            for (MissingChangeInfo info : entry.getValue())
+//                System.out.printf("(Line%d, Line%d), %s\n", info.getStartLine(), info.getEndLine(), info.getContent());
+//        }
 
     }
 }
