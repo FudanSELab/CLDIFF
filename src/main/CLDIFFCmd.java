795786f82a0a2b8d8af9a36d390a8d43db147609
JunYuWang0808 <jwang2289@wisc.edu>
2022-06-03 13:12:52 -0700

    test patch 1

--- src/main/CLDIFFCmd.java
+++ src/main/CLDIFFCmd.java
@@ -10,11 +10,11 @@ public class CLDIFFCmd {
 
     public static void main(String args[]){
         Global.runningMode = 0;
-        String repo = "../CLIDIFFTEST/.git";
-        String commitId = "9dfd6f7e97adf435c8d3bfe61d42b43e0b4e0713";
+//        String repo = "../CLIDIFFTEST/.git";
+//        String commitId = "9dfd6f7e97adf435c8d3bfe61d42b43e0b4e0713";
         // https://github.com/JunYuWang0808/CLIDIFFTEST
-        //String repo = "/Users/junyu/Desktop/2022spring/cs230/Project/CLIDIFFTEST/.git";
-        //String commitId = "9dfd6f7e97adf435c8d3bfe61d42b43e0b4e0713";
+        String repo = "/Users/junyu/Desktop/2022spring/cs230/Project/TestSet/TestPatch1/.git";
+        String commitId = "394f7a04f68e65c23aa450b615eb6ae89c9b7422";
         String outputDir = "./output";
         CLDiffLocal CLDiffLocal = new CLDiffLocal();
         CLDiffLocal.run(commitId,repo,outputDir);
